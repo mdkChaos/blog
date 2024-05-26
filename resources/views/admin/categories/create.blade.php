999b76c9934a9a1980e2d20f9d901aad013e2a87
mdkChaos <mdkchaos1@gmail.com>
2024-05-26 22:39:03 +0200

    Add Category controller and view

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,12 +8,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Dashboard</h1>
+                        <h1 class="m-0">Add category</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item active">Add category</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
@@ -26,11 +26,15 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-1">
-                        <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Add</a>
-                    </div>
                     <div class="col-12">
-                        Categories
+                        <form action="#" method="post" class="form w-25">
+                            @csrf
+                            <div class="form-group">
+                                <label>Name category</label>
+                                <input type="text" name="" class="form-control" placeholder="Name category">
+                            </div>
+                            <input type="submit" value="Add" class="btn btn-primary col-4">
+                        </form>
                     </div>
                 </div>
                 <!-- /.row -->
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\Main\IndexController as MainIndexController;
 use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexController;
+use App\Http\Controllers\Admin\Category\CreateController as CategoryCreateController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', MainIndexController::class);
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/', AdminIndexController::class);
 
     Route::prefix('categories')->group(function () {
-        Route::get('/', CategoryIndexController::class);
+        Route::get('/', CategoryIndexController::class)->name('category.index');
+        Route::get('/create', CategoryCreateController::class)->name('category.create');
     });
 });
 
