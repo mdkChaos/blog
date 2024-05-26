68b48426cf8377d3bd734f007088490e227c1b71
mdkChaos <mdkchaos1@gmail.com>
2024-05-26 20:54:10 +0200

    add AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
-use App\Http\Controllers\Main\IndexController;
+use App\Http\Controllers\Main\IndexController as MainIndexController;
+use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::group([], function () {
-    Route::get('/', IndexController::class);
+Route::get('/', MainIndexController::class);
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', AdminIndexController::class);
 });
 
 Auth::routes();
