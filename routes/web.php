0aa4d30a30f72be8a7e988cac08b9de92d5f9fac
mdkChaos <mdkchaos1@gmail.com>
2024-05-26 21:48:18 +0200

    Add CategoryController and view

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,17 @@
 
 use App\Http\Controllers\Main\IndexController as MainIndexController;
 use App\Http\Controllers\Admin\Main\IndexController as AdminIndexController;
+use App\Http\Controllers\Admin\Category\IndexController as CategoryIndexController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', MainIndexController::class);
 
 Route::prefix('admin')->group(function () {
     Route::get('/', AdminIndexController::class);
+
+    Route::prefix('categories')->group(function () {
+        Route::get('/', CategoryIndexController::class);
+    });
 });
 
 Auth::routes();
