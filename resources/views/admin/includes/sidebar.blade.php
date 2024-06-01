27d9832a8cbb889e50958bf7d83f7cb3777da80a
mdkChaos <mdkchaos1@gmail.com>
2024-06-01 19:02:15 +0200

    refactoring

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -10,15 +10,15 @@
                 <a href="{{ route('admin.category.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-th-list"></i>
                     <p>
-                        Category
+                        Categories
                     </p>
                 </a>
             </li>
             <li class="nav-item">
                 <a href="{{ route('admin.tag.index') }}" class="nav-link">
-                    <i class="nav-icon fas fa-th-list"></i>
+                    <i class="nav-icon fas fa-tags"></i>
                     <p>
-                        Tag
+                        Tags
                     </p>
                 </a>
             </li>
--- resources/views/admin/tag/show.blade.php
+++ resources/views/admin/tag/show.blade.php
@@ -10,6 +10,13 @@
                     <div class="col-sm-6 d-flex align-items-center">
                         <h1 class="m-0 mr-2">{{ $tag->title }}</h1>
                         <a href="{{ route('admin.tag.edit', $tag->id) }}" class="fas fa-edit text-success"></a>
+                        <form action="{{ route('admin.tag.delete', $tag->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="border-0 bg-transparent">
+                                <i class="fas fa-trash-alt text-danger" role="button"></i>
+                            </button>
+                        </form>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
