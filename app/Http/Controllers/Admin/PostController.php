930085d409b706f3c78b91eb6e0079cda1e9be40
Andrii Poludniev <mdkchaos1@gmail.com>
2024-06-08 21:15:26 +0200

    Add Service

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -8,10 +8,18 @@ use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\Tag;
-use Illuminate\Support\Facades\Storage;
+use App\Service\PostService;
 
 class PostController extends Controller
 {
+    public $service;
+
+    public function __construct(PostService $service)
+    {
+        $this->service = $service;
+    }
+
+
     /**
      * Display a listing of the resource.
      */
@@ -39,19 +47,8 @@ class PostController extends Controller
      */
     public function store(StoreRequest $request)
     {
-        try {
-            $data = $request->validated();
-            $tagIds = $data['tag_ids'];
-            unset($data['tag_ids']);
-
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-
-            $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagIds);
-        } catch (\Exception $exception) {
-            abort(404);
-        }
+        $data = $request->validated();
+        $this->service->store($data);
 
         return redirect()->route('admin.post.index');
     }
@@ -82,21 +79,9 @@ class PostController extends Controller
     {
         $data = $request->validated();
 
-        $tagIds = $data['tag_ids'];
-        unset($data['tag_ids']);
+        $result = $this->service->update($data, $post);
 
-        if (isset($data['preview_image'])) {
-            $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
-        }
-
-        if (isset($data['main_image'])) {
-            $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
-        }
-
-        $post->update($data);
-        $post->tags()->sync($tagIds);
-
-        return redirect()->route('admin.post.show', compact('post'));
+        return redirect()->route('admin.post.show', compact('result'));
     }
 
     /**
