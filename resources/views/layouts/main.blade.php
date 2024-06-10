9e90cf9f35ec5e28e56dca00345a12301a3c0fec
mdkChaos <mdkchaos1@gmail.com>
2024-06-10 22:12:59 +0200

    Refactoring

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -78,15 +78,39 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item ml-auto">
+                            @if (Auth::user()->role === App\Models\User::ROLE_ADMIN)
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('admin.index') }}">Panel</a>
+                                </li>
+                            @endif
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            {{-- <li class="nav-item">
                                 <p class="mb-0">{{ Auth::user()->name }}</p>
                             </li>
-                            <li class="nav-item ml-3">
+                            <li class="nav-item ml-2">
                                 <form action="{{ route('logout') }}" method="POST">
                                     @csrf
                                     <input class="btn btn-outline-primary" type="submit" value="Logout">
                                 </form>
-                            </li>
+                            </li> --}}
                         @endguest
                     </ul>
                 </div>
