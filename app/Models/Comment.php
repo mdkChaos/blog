783ca3973a4a69ac393efc5954b358f24b64a1e3
Andrii Poludniev <mdkchaos1@gmail.com>
2024-06-15 23:19:36 +0200

    refactoring

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
@@ -16,13 +17,13 @@ class Comment extends Model
         'message',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'user_id', 'id');
+        return $this->belongsTo(User::class);
     }
 
-    public function getDateAsCarboneAttribute()
+    protected function dateAsCarbon(): Attribute
     {
-        return Carbon::parse($this->created_at);
+        return Attribute::get(fn () => $this->created_at->toImmutable());
     }
 }
--- resources/views/components/post/comment/comment.blade.php
+++ resources/views/components/post/comment/comment.blade.php
@@ -4,11 +4,10 @@
     <div class="comment-text">
         <span class="username">
             <b>{{ $comment->user->name }}</b>
-            <span class="text-muted float-right">{{ $comment->date_as_carbone->diffForHumans() }}</span>
-        </span><!-- /.username -->
+            <span class="text-muted float-right">{{ $comment->date_as_carbon->diffForHumans() }}</span>
+        </span>
         <p>
             {{ $comment->message }}
         </p>
     </div>
-    <!-- /.comment-text -->
 </div>
